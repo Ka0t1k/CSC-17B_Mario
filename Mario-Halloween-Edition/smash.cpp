fce36878f88d6f261049f56f51ad25fc38f2ba8b
equati0n <jbaird1@student.rcc.edu>
2016-10-05 20:06:33 -0700

    Delete smash.cpp

--- Mario-Halloween-Edition/smash.cpp
+++
@@ -1,7 +0,0 @@
-#include "smash.h"
-
-Smash::Smash(int x, int y) : Brick(x,y)
-{
-    this->sprite = QPixmap(":images/Smash.png");
-    this->setRect(QRect(x, y, sprite.width(), sprite.height() / 3));
-}
