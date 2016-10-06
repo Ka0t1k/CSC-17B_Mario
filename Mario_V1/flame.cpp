caa81b77ad553c5a1d7b423418807fea2951f6fe
equati0n <jbaird1@student.rcc.edu>
2016-10-05 19:55:00 -0700

    Delete flame.cpp

--- Mario_V1/flame.cpp
+++
@@ -1,8 +0,0 @@
-#include "flame.h"
-
-Flame::Flame(int x, int y) : Brick(x,y)
-{
-    this->setSprite(QString(":images/fire.png"));
-    this->rect = QRect(x, y -10, getSprite().width()/9, getSprite().height());
-}
-
