be054afed6c19b2480302629154b7fc3b4d33225
nn2410841 <nnguyen37@student.rcc.edu>
2016-10-15 18:39:29 -0700

    Position Fix

--- gameMainWindow/title.cpp
+++ gameMainWindow/title.cpp
@@ -12,17 +12,17 @@ Title::Title(View *view, QWidget *parent) : QGraphicsScene(parent){
     start->setPlainText("S t a r t  G a m e");
     start->setDefaultTextColor(Qt::black);
     start->setFont(font);
-    start->setPos((width - start->boundingRect().width()) / 2, height - 250);
+    start->setPos((width - start->boundingRect().width()) / 2, height - 275);
 
     quit = new QGraphicsTextItem;
     quit->setPlainText("Q u i t  G a m e");
     quit->setDefaultTextColor(Qt::black);
     quit->setFont(font);
-    quit->setPos((width - quit->boundingRect().width()) / 2, height - 200);
+    quit->setPos((width - quit->boundingRect().width()) / 2, height - 275 + start->boundingRect().height());
 
-    cursor->setPos((width - start->boundingRect().width()) / 2 - cursor->boundingRect().width(), height - 250 + cursor->boundingRect().height() / 4);
+    cursor->setPos((width - start->boundingRect().width()) / 2 - cursor->boundingRect().width(), (height - 275 + start->boundingRect().height() / 4));
     logo->setPos((width - logo->boundingRect().width()) / 2, 50);
-
+    std::cout<<cursor->boundingRect().height();
     addItem(background);
     addItem(logo);
     addItem(cursor);
@@ -37,19 +37,19 @@ void Title::keyPressEvent(QKeyEvent *event){
     if(event->key() == Qt::Key_Up){
         if(selection == 0){
             selection++;
-            cursor->moveBy(0,50);
+            cursor->moveBy(0,start->boundingRect().height());
         } else{
             selection--;
-            cursor->moveBy(0,-50);
+            cursor->moveBy(0,-start->boundingRect().height());
         }
     }
     if(event->key() == Qt::Key_Down){
         if(selection == 1){
             selection--;
-            cursor->moveBy(0,-50);
+            cursor->moveBy(0,-start->boundingRect().height());
         } else{
             selection++;
-            cursor->moveBy(0,50);
+            cursor->moveBy(0,start->boundingRect().height());
         }
     }
     if(event->key() == Qt::Key_Z){
