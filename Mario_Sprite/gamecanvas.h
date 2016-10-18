a027ca6273891bb3c5289e04bd17e4ec2c2696a8
equati0n <jbaird1@student.rcc.edu>
2016-10-17 18:24:47 -0700

    Add files via upload

---
+++ Mario_Sprite/gamecanvas.h
@@ -0,0 +1,26 @@
+#ifndef GAMECANVAS_H
+#define GAMECANVAS_H
+
+#include <QWidget>
+
+#include "mariosprite.h"
+
+class GameCanvas : public QWidget
+{
+    Q_OBJECT
+
+public:
+    GameCanvas( QWidget* parent= 0 );
+    ~GameCanvas();
+    void paintEvent(QPaintEvent *painter);
+    void timerEvent(QTimerEvent *);
+
+public slots:
+
+
+
+private:
+    MarioSprite* m_mario;
+};
+
+#endif // GAMECANVAS_H
