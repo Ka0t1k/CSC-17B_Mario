ae46d2fb0f60507e362ce5cd47c2c1616fcd0e77
equati0n <jbaird1@student.rcc.edu>
2016-11-09 15:27:30 -0800

    Brick_Collision+Coin Sound

---
+++ Brick_Collision/questbox.h
@@ -0,0 +1,20 @@
+#ifndef QUESTBOX_H
+#define QUESTBOX_H
+#include <QGraphicsItem>
+#include <QPixmap>
+
+class QuestBox : public QGraphicsItem
+{
+public:
+    QuestBox(QGraphicsItem *parent = 0);
+    void nextFrame4();
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+private:
+
+    int mCurrentFrame4;
+    QPixmap mPixmap4;
+
+};
+#endif // QUESTBOX_H
