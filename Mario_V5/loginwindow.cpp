57d3644254833a2a2bc6813b444c8e5d47b903a9
equati0n <jbaird1@student.rcc.edu>
2016-12-05 03:44:50 -0800

    Game + Added Password Echo Mode

---
+++ Mario_V5/loginwindow.cpp
@@ -0,0 +1,127 @@
+#include "loginwindow.h"
+#include "ui_loginwindow.h"
+
+
+LoginWindow::LoginWindow(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::LoginWindow)
+{
+    ui->setupUi(this);
+}
+
+LoginWindow::~LoginWindow(){
+    delete ui;
+}
+
+void LoginWindow::connectDatabase()
+{
+    Connection connection = createConnection();
+    if ( !connection.connected ){
+        qDebug() << "Not connected!";
+        //return 1;
+    }
+    else{
+        qDebug() << "Connected!";
+        QSqlQuery query;
+        query.exec("SELECT username FROM mario_table");
+
+        while (query.next()) {
+            QString name = query.value(0).toString();
+            qDebug() << "name:" << name;
+
+        }
+        connection.db.close();
+        //return 0;
+    }
+
+}
+
+
+void LoginWindow::on_pushButton_clicked()
+{
+//Connect ot Database
+    createConnection();
+//Gather Input
+    Query uInput;
+    uInput.uName=ui->USERNAME->text();
+    uInput.pass=ui->PASSWORD->text();
+    qDebug() << uInput.uName;
+    qDebug() << uInput.pass;
+//Try to Register User
+    if(regUsr(uInput)){
+        //Then it worked
+    }
+    else{
+        //Then it didnt
+    }
+}
+
+Connection LoginWindow::createConnection(){
+    db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("209.129.8.7");
+    db.setDatabaseName("48037");
+    db.setUserName("48037");
+    db.setPassword("48037cis17b");
+    Connection connection;
+    connection.db = db;
+    if (!db.open()) {
+        qDebug() << "Database error occurred";
+        connection.connected = false;
+        return connection;
+    }
+    connection.connected = true;
+
+    return connection;
+}
+
+//Register User
+bool LoginWindow::regUsr(Query Input){
+//Check user input against the database
+    QString insert;
+    QSqlQuery Q(db);
+    if(chkUsr(Input)){
+    //Define SQL Query
+        insert="INSERT INTO mario_table (username,password) VALUES ('"+Input.uName+"', SHA1('"+Input.pass+"'))";
+        Q.exec(insert);
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+//Check if Users are in Database
+bool LoginWindow::chkUsr(Query Input){
+    QSqlQuery Q(db);
+    QString select;
+//Setup a boolean
+    bool valid=true;
+//Define the Query
+    select="SELECT username FROM mario_table WHERE username='"+Input.uName+"'";
+    Q.exec(select);
+    if(Q.size()==1){
+        //Username Taken
+        return false;
+    }
+    else
+        return valid;
+}
+
+
+void LoginWindow::on_pushButton_2_clicked()
+{
+    LoginWindow::reject();
+}
+
+void LoginWindow::on_radioButton_clicked(bool checked)
+{
+    if(checked){
+        ui->PASSWORD->setEchoMode(QLineEdit::Normal);
+    }
+    else{
+        ui->PASSWORD->setEchoMode(QLineEdit::Password);
+    }
+}
+
+
+
---
+++ Mario_V5/title.h
@@ -0,0 +1,61 @@
+#ifndef TITLE_H
+#define TITLE_H
+#include <QPushButton>
+#include <QLineEdit>
+#include <QRadioButton>
+#include "mainwindow.h"
+#include "myscene.h"
+class View;
+class LoginWindow;
+class AnimatedGraphicsItem : public QObject, public QGraphicsPixmapItem{
+    Q_OBJECT
+    Q_PROPERTY(QPointF pos READ pos WRITE setPos)
+};
+
+class Title : public QGraphicsScene{
+    Q_OBJECT
+public:
+    Title(View *view, QWidget *parent = 0);
+
+signals:
+    void quitGame();
+    void playSound(QString);
+protected:
+    void keyPressEvent(QKeyEvent *event);
+
+private slots:
+     void login();
+     void newUser();
+
+    void on_radioButton_toggled(bool checked);
+private:
+    int selection = 0;
+    const int width = 1280;
+    const int height = 720;
+    AnimatedGraphicsItem *background;
+    View *viewer;
+    QGraphicsPixmapItem *foreground;
+    QGraphicsPixmapItem *cursor;
+    QGraphicsPixmapItem *logo;
+    QGraphicsPixmapItem *border;
+    QGraphicsTextItem *start;
+    QGraphicsTextItem *quit;
+    QMediaPlaylist *playlist;
+    QPropertyAnimation *animation;
+    QScrollBar *scroll;
+    MyScene *scene;
+    QPushButton *loginButton;
+    QPushButton *newUserButton;
+    LoginWindow *loginWindow;
+    QLineEdit * userLine;
+    QLineEdit * passLine;
+    QLabel *userName;
+    QLabel *radioText;
+
+    QLabel *password;
+    QRadioButton *radioButton;
+
+
+};
+
+#endif // TITLE_H
